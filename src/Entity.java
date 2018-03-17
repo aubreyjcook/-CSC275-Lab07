8e7a0604b213f02793f1339b763e11543548ab53
aubreyjcook <aubreyjcook@gmail.com>
2018-03-16 21:40:23 -0500

    entity class created, getter/setter methods created

--- src/Entity.java
+++ src/Entity.java
@@ -1,4 +1,42 @@
 
 public class Entity {
+	static String strName;
+	static int intLife;
+	static int intEnergy;
+	
+	Entity(){}
+	
+	//constructor
+	public Entity(String entityName, int entityLife, int entityEnergy) {
+		Entity.strName   = entityName;
+		Entity.intLife   = entityLife;
+		Entity.intEnergy = entityEnergy;
+	}
+	
+	//setter methods
+	public void setStrName(String name) {
+		Entity.strName = name;
+	}
+	
+	public void setIntLife(int life) {
+		Entity.intLife = life;
+	}
+	
+	public void setIntEnergy(int energy) {
+		Entity.intEnergy = energy;
+	}
+	
+	//getter methods
+	public String getStrName() {
+		return strName;
+	}
+	
+	public int getIntLife() {
+		return intLife;
+	}
+	
+	public int getIntEnergy() {
+		return intEnergy;
+	}
 
 }
